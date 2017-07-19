dd8650012fd5c58f8670d3bac124093362ddc8fc
SoonSem <qkeh4161@naver.com>
2017-07-19 22:26:56 +0900

    TopCoder algorithm training.  this problem is so hard to understand..

---
+++ 회문.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ThePalindrome
+{
+    public int find(string s)
+    {
+        
+        for (int i = s.Length; ; i++)
+        {
+            bool flag = true;
+
+            for (int j = 0; j < s.Length; j++)
+            {
+                if(s[j] != s[i - j - 1] && i - j - 1 < s.Length)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (false) return i;
+        }
+    }
+}
