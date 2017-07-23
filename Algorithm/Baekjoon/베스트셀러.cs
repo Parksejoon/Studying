87794872cc10a3a610800e108d2c0e88ac26c447
SoonSem <qkeh4161@naver.com>
2017-07-23 17:14:26 +0900

    baekjoon 1302

---
+++ Algorithm/Baekjoon/베스트셀러.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace maincode
+{
+    class mainApp
+    {
+        static void Main()
+        {
+            Dictionary<string, int> arr = new Dictionary<string, int>();
+            int n = int.Parse(Console.ReadLine());
+            string[] temp = new string[1000];
+
+            for (int i = 0; i < n; i++)
+            {
+                temp[i] = Console.ReadLine();
+                arr[temp[i]] = 0;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[temp[i]]++;
+            }
+
+            string big = temp[0];
+
+            foreach (KeyValuePair<string, int> kv in arr)
+            {
+                if (arr[big] < arr[kv.Key])
+                {
+                    big = kv.Key;
+                }
+                else if (arr[big] == arr[kv.Key])
+                {
+                    if (string.Compare(kv.Key, big) < 0)
+                    {
+                        big = kv.Key;
+                    }
+                }
+            }
+
+            Console.WriteLine(big);
+        }
+    }
+}
