9cdefb54f0892b237f44bcfc64f2f3bfad229fa5
SoonSem <qkeh4161@naver.com>
2017-08-06 10:12:39 +0900

    TopCoderAlgorithmTraining NumberMagicEasy

---
+++ Algorithm/TopCoderAlgorithmTraining/마법의 숫자.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class NumberMagicEasy
+{
+    static void Main()
+    {
+        int result = theNumber(Console.ReadLine());
+        Console.WriteLine(result);
+    }
+
+    public static int theNumber(string answer)
+    {
+        string[] arr =
+        {
+            "YYYY",
+            "YYYN",
+            "YYNY",
+            "YYNN",
+            "YNYY",
+            "YNYN",
+            "YNNY",
+            "YNNN",
+            "NYYY",
+            "NYYN",
+            "NYNY",
+            "NYNN",
+            "NNYY",
+            "NNYN",
+            "NNNY",
+            "NNNN"
+        };
+
+        for (int i = 0; i < 16; i++)
+        {
+            if (arr[i] == answer) return i + 1;
+        }
+
+        return 0;
+    }
+}
