85fdfd215c48fa5b4342653f9378705950b6a516
SoonSem <qkeh4161@naver.com>
2017-08-16 23:56:13 +0900

    TopCoderAlgorithmTraining Dynamic programming

---
+++ Algorithm/TopCoderAlgorithmTraining/동적 계획법 기본_1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Studying_CS
+{
+    class 동적계획법_1
+    {
+        const int h = 5, w = 4;
+        int[,] dp = new int[h + 1, w + 1];
+
+        int dfs(int nowh, int noww)
+        {
+            if (noww > w || nowh > h) return 0;
+            if (noww == w || nowh == h) return 1;
+            if (dp[nowh, noww] != 0) return dp[nowh, noww];
+            return dp[nowh, noww] = dfs(nowh + 1, noww) + dfs(nowh, noww + 1);
+        }
+    }
+}
---
+++ Algorithm/TopCoderAlgorithmTraining/동적 계획법 기본_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Studying_CS
+{
+    class 동적계획법_2
+    {
+        const int h = 5, w = 4;
+        int[,] dp = new int[h + 1, w + 1];
+
+        void calc()
+        {
+            int i, j;
+            dp[0, 0] = 1;
+
+            for (i = 0; i <= h; i++)
+            {
+                for (j = 0; j <= w; j++)
+                {
+                    if (i != 0) dp[i, j] += dp[i - 1, j];
+                    if (j != 0) dp[i, j] += dp[i, j - 1];
+                }
+            }
+        }
+    }
+}
