b56656bc35b6d456f62d138fa9b803476d5607cf
SoonSem <qkeh4161@naver.com>
2017-08-17 15:11:32 +0900

    TopCoderAlgorithmTraining Knapsack

---
+++ Algorithm/TopCoderAlgorithmTraining/배낭_동적계획법.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Studying_CS
+{
+    class 배낭_동적계획법
+    {
+        int[] ws = { 3, 4, 1, 2, 3 };
+        int[] ps = { 2, 3, 2, 3, 6 };
+        int maxw = 10;
+        int[,] dp = new int[6, 11];
+        int ret = 0;
+        
+        void knapsack()
+        {
+            for (int i = 0; i <= ws.Length; i++)
+            {
+                for (int j = 0; j <= maxw; j++)
+                {
+                    if (j + ws[i] <= maxw)
+                    {
+                        dp[i + 1, j + ws[i]] = Math.Max(dp[i + 1, j + ws[j]], dp[i, j] + ps[i]);
+                        ret = Math.Max(dp[i + 1, j + ws[i]], ret);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Algorithm/TopCoderAlgorithmTraining/배낭_메모화 재귀.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Studying_CS
+{
+    class 배낭_메모화_재귀
+    {
+        int[] ws = new int[] { 3, 4, 1, 2, 3 };
+        int[] ps = new int[] { 2, 3, 2, 3, 6 };
+        int maxw = 10;
+        int[,] dp = new int[6, 11];
+
+        int knapsack(int n, int w)
+        {
+            if (w > maxw) return -1;
+            if (n >= ws.Length) return 0;
+            if (dp[n, w] >= 0) return dp[n, w];
+            return dp[n, w] = Math.Max(knapsack(n + 1, w), knapsack(n + 1, w + ws[n]) + ps[n]);
+        }
+    }
+}
