a4ebd39fec50f91c7b39650ca1738e14f4a8c1c0
LAPTOP-SoonSem <qkeh4161@naver.com>
2019-03-16 21:21:43 +0900

    Add comment in sorting algorithm

--- Algorithm/Datastruct/MergeSort.cpp
+++ Algorithm/Datastruct/MergeSort.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// 
 void merge(int* arr, int left, int mid, int right)
 {
 	int i, j, k, m;
@@ -14,6 +15,7 @@ void merge(int* arr, int left, int mid, int right)
 
 	int temp_arr[SIZE];
 
+	// 한쪽 배열을 다 쓸 때까지 병합
 	while (i <= mid && j <= right)
 	{
 		if (arr[i] < arr[j])
@@ -26,6 +28,7 @@ void merge(int* arr, int left, int mid, int right)
 		}
 	}
 
+	// 남아있는 배열을 뒤에 붙여줌
 	if (i > mid)
 	{
 		for (m = j; m <= right; m++)
@@ -41,22 +44,29 @@ void merge(int* arr, int left, int mid, int right)
 		}
 	}
 
+	// 정렬된 배열을 리턴
 	for (m = left; m <= right; m++)
 	{
+		cout << temp_arr[m] << " ";
 		arr[m] = temp_arr[m];
 	}
+	cout << endl;
 }
 
 void merge_sort(int* arr, int left, int right)
 {
 	int mid;
 
+	// 배열의 길이가 1이하이면 소팅 안함
 	if (left < right)
 	{
+		// 중간지점 잡고
 		mid = (left + right) / 2;
 
+		// 반으로 나눠서 재귀
 		merge_sort(arr, left, mid);
 		merge_sort(arr, mid + 1, right);
+		// 반으로 나누어진걸 합침(소팅)
 		merge(arr, left, mid, right);
 	}
 }
@@ -65,8 +75,5 @@ int main(void)
 {
 	int arr[SIZE] = { 21, 10, 12, 20, 25, 13, 15 ,22, 42, 11 };
 
-	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
-	{
-		cout << arr[i] << " ";
-	}
+	merge_sort(arr, 0, 9);
 }
--- Algorithm/Datastruct/QuickSort.cpp
+++ Algorithm/Datastruct/QuickSort.cpp
@@ -2,19 +2,26 @@
 
 using namespace std;
 
+// 이방식으로 할때 생각나는 문제점 
+// 1. 데이터 소멸의 문제
+//		멋대로 스왑도 안하고 pivot위치나 r위치에 값을 넣으면 데이터가 소멸하지 않을까?
+//		=> 만약 데이터가 소멸할려면 l, r가 두번 땡겨져야 하는데, 그런 경우는 있을 수 없음
 void qsort(int* arr, int left, int right)
 {
+	// 만약 배열의 길이가 1이하면
 	if (left > right)
 	{
 		return;
 	}
 
+	// l, r, pivot값을 초기화
 	int l = left;
 	int r = right;
 	int p = arr[left];
 
 	while (l < r)
 	{
+		// r을 pivot 보다 작은 값을 만날 때 까지 땡김
 		while (arr[r] >= p && l < r)
 		{
 			r--;
@@ -23,9 +30,11 @@ void qsort(int* arr, int left, int right)
 		// 만약 현재 값이 피봇보다 작으면
 		if (l != r)
 		{
+			// pivot에 r값을 대입
 			arr[left] = arr[r];
 		}
 
+		// l을 pivot 보다 큰 값을 만날 때 까지 땡김
 		while (arr[l] <= p && l < r)
 		{
 			l++;
@@ -34,14 +43,19 @@ void qsort(int* arr, int left, int right)
 		// 만약 현재 값이 피봇보다 크면
 		if (l != r)
 		{
+			// r에 l값을 대입
 			arr[r] = arr[l];
+			// 대입을 하면서 값이 변질됬기에 왼쪽으로 한 칸 땡김
 			r--;
 		}
 	}
 
+	// l에 pivot값을 대입
 	arr[l] = p;
+	// pivot을 l로 치환
 	p = l;
 	
+	// 분할해서 qsort
 	qsort(arr, left, p - 1);
 	qsort(arr, p + 1, right);
 
